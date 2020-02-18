0e82161236b212de0fb6d8710c9c82b8462e689b
SL\msa <msa@tiqri.com>
2020-02-18 08:50:52 +0300

    adding project

---
+++ ForumMVC/Services/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ForumMVC.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
